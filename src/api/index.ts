9fa9019f38a3cbb3819ee2111f718386f4fef9dd
Abdelrahman Soliman <bebofit@aucegypt.edu>
2020-03-29 00:35:19 +0200

    small fixes

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authRoutes from './auth';
 import usersRoutes from './users';
 import carpetsRoutes from './carpets';
+import trailsRoutes from './trials';
 import movingStocksRoutes from './moving-stock';
 import { isAuthenticated } from '../common/middleware';
 
@@ -11,5 +12,6 @@ router.use('/auth', authRoutes);
 router.use('/users', usersRoutes);
 router.use('/carpets', isAuthenticated, carpetsRoutes);
 router.use('/moving-stocks', isAuthenticated, movingStocksRoutes);
+router.use('/trials', isAuthenticated, trailsRoutes);
 
 export default router;
